9476870594ae19d13efeaaf35b09b37b5d7cd995
nodo-o <qmoop.robi@gmail.com>
2024-01-03 14:25:33 +0100

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ function Header() {
     return (
         <div className="header">
             <div className="logo-container">
-                <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="Logo" className="logo" />
+                <img src={`${process.env.PUBLIC_URL}/iwy-logo.png`} alt="Logo" className="logo" />
             </div>
             <div className="title-container">
-                <h1>My React Page</h1>
+                <h2>It was you</h2>
             </div>
         </div>
     );
