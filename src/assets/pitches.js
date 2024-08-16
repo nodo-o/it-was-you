409ae0c635e01d668183391545bcaa0e0b02a3ad
nodo-o <qmoop.robi@gmail.com>
2024-08-16 14:52:34 +0200

    add config for mobile

--- src/assets/pitches.js
+++ src/assets/pitches.js
@@ -1,8 +1,8 @@
-export const FOURSEAM = { name: '4-Seamer',
+export const FOURSEAM = { name: '4Seamer',
     description: 'fastball bla bla bla',
     photo: '/pitches/4seamfastball.png',
 }
-export const TWOSEAM = { name: '2-Seamer',
+export const TWOSEAM = { name: '2Seamer',
     description: 'fastball bla bla bla',
     photo: '/pitches/2seam.png',
 }
