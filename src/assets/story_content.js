0769060b02777c7b1e76e0306c9cfcbb912cf6fe
nodo-o <qmoop.robi@gmail.com>
2024-09-17 16:27:03 +0200

    add components tory

--- src/assets/story_content.js
+++ src/assets/story_content.js
@@ -1,6 +1,13 @@
 
-let intro = "Former high school teammates Kuramochi Youichi and Miyuki Kazuya find themselves back together at Meiji University, dealing with the pressures of being top-tier athletes while trying to figure out life off the field. Kuramochi is all about keeping things simple—until a dating app throws his emotional stability out of the window. Meanwhile, Miyuki, the team’s captain and baseball prodigy, struggles to keep his cool as he faces his past, his future, and his complicated feelings for the only person he's being willing to yield control to. This story is about more than just baseball—it's about navigating friendship, love, and figuring out whom you really are when the game’s over and the crowd is gone."
+let intro = "Former high school teammates Kuramochi Youichi and Miyuki Kazuya find themselves back together at Meiji University, dealing with the pressures of being top-tier athletes while trying to figure out life off the field." +
+    "<br><br>Kuramochi is all about keeping things simple—until a dating app throws his emotional stability out of the window. Meanwhile, Miyuki, the team’s captain and baseball prodigy, struggles to keep his cool as he faces his past, his future, and his complicated feelings for the only person he's being willing to yield control to. " +
+    "<br><br>This story is about more than just baseball—it's about navigating friendship, love, and figuring out whom you really are when the game’s over and the crowd is gone."
 
 
-export const STORY = { intro: intro
+export const STORY = {
+    url: "https://archiveofourown.org/works/45155977",
+    nume_url:"https://x.com/nume_x/status/1782887284209287300",
+    title: "it was you",
+    intro: intro,
+    buttons: ["author: nodo", "rating: E", "status: ongoing"]
 }
--- src/components/StoryComponent.js
+++ src/components/StoryComponent.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './TextContent.css';
+import './StoryComponent.css';
+import {STORY} from '../assets/story_content';
 
 function StoryComponent() {
     return (
         <div className="text-content">
+            <div className="title-block">
+                <h1>{STORY.title}</h1>
+            </div>
+            <div className="summary-block" dangerouslySetInnerHTML={{__html: STORY.intro}}></div>
+            <div className="buttons-block">
+                {STORY.buttons.map((buttonText, index) => (
+                    <button key={index}>{buttonText}</button>
+                ))}
+            </div>
+            <a href={STORY.url} target="_blank" className="ao3-link">
+                <img src={`${process.env.PUBLIC_URL}${"/ao3.svg"}`} alt="AO3" className="ao3-icon"/>
+                <span><em>Read it on AO3!</em></span>
+            </a>
+            <a href={STORY.nume_url} target="_blank">
+                <img src={`${process.env.PUBLIC_URL}${"/nume-art.jpg"}`} alt="Story" className="full-width-image"/>
+            </a>
         </div>
-    );
+    )
+        ;
 }
 
 export default StoryComponent;
